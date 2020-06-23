5c0a72db14f1d9dbe4d653f0c7f49f10058134c3
Guillermo De Leon <guillermo.x.deleon@gmail.com>
2020-06-22 22:36:26 -0500

    working java game

--- src/clown/game/Game.java
+++ src/clown/game/Game.java
@@ -1,4 +1,81 @@
 package clown.game;
 
-public class Game {
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+
+public class Game extends Canvas implements Runnable{
+
+    Thread thread;
+    private boolean isRunning = false;
+
+    public Game(){
+        new Window(640, 480, "Java Clown Course", this);
+        start();
+    }
+
+    private void start(){
+        isRunning = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    private void stop(){
+        isRunning = false;
+        try {
+            thread.join();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void run(){
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while(isRunning){
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while(delta >= 1){
+                tick();
+                delta--;
+            }
+            if(isRunning)
+                render();
+            frames++;
+
+            if(System.currentTimeMillis() - timer > 1000){
+                timer += 1000;
+                System.out.println("FPS: " + frames);
+                frames =0;
+            }
+        }
+    }
+
+    public void tick(){
+
+    }
+
+    public void render(){
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs == null){
+            this.createBufferStrategy(3);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+
+        g.setColor(Color.green);
+        g.fillRect(0,0, 640,480);
+
+        g.dispose();
+        bs.show();
+
+    }
+    public static void main(String args[]){
+        new Game();
+
+    }
 }
